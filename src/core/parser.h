6971cd8e68b5de5a14d16d70a9dae40561378423
Peter Arthur <pete.arthur@gmail.com>
2019-06-08 10:28:35 +0100

    Start string handling

--- src/core/parser.h
+++ src/core/parser.h
@@ -31,29 +31,57 @@ typedef int (*Consumer)(Parser *p, ParseState *state, uint8_t c);
 struct ParseState {
     int32_t counter;
     int32_t argn;
-    int flags;
+    uint32_t flags;
     size_t start;
     Consumer consumer;
 };
 
 /* The Fenn parser structure */
 struct Parser {
-    const char *error;  // The current error
-    ParseState *states; // Store the stack of ParseStates
-    uint8_t *buffer;    // The buffer we are currently parsing
-    size_t statecount;  // Number of states on the stack
-    size_t statecap;    // Amount of memory allocated for states
-    int offset;         // Stores the offset into the buffer we are parsing
-    int lineno;         // The current line number
-    int colno;          // The current column number
+    const char *error;   // The current error
+    ParseState *states;  // Store the stack of ParseStates
+
+    size_t statecount;   // Number of states on the stack
+    size_t statecap;     // Number of states allocated
+    uint8_t *buffer;     // The buffer we are currently parsing
+    size_t  buffercount; // Size of the current buffer
+    size_t  buffercap;   // Capacity of the buffer
+    int offset;          // Stores the current offset into the buffer we are parsing
+    int lineno;          // The current line number
+    int colno;           // The current column number
 };
+
+/* Flags */
+#define PFLAG_CONTAINER     ((uint32_t)0x100)
+#define PFLAG_BUFFER        ((uint32_t)0x200)
+#define PFLAG_PARENS        ((uint32_t)0x400)
+#define PFLAG_SQRBRACKETS   ((uint32_t)0x800)
+#define PFLAG_CURLYBRACKETS ((uint32_t)0x1000)
+#define PFLAG_STRING        ((uint32_t)0x2000)
+#define PFLAG_LONGSTRING    ((uint32_t)0x4000)
+#define PFLAG_READERMAC     ((uint32_t)0x8000)
+#define PFLAG_ATSYM         ((uint32_t)0x10000)
+#define PFLAG_INSTRING      ((uint32_t)0x100000)
+#define PFLAG_END_CANDIDATE ((uint32_t)0x200000)
 
 /* Function declarations */
 int is_whitespace(uint8_t);
 int is_symbol_char(uint8_t);
 
 void pushstate(Parser *, Consumer, int);
+void pushbuffer(Parser *, uint8_t);
 
+/* Consumers */
 int expression(Parser *, ParseState *, uint8_t);
+int stringchar(Parser *, ParseState *, uint8_t);
+int escape(Parser *, ParseState *, uint8_t);
+int escapehex(Parser *, ParseState *, uint8_t);
+
+/* Utility functions */
+int stringend(Parser *, ParseState *);
+
+/* Creation and destruction */
+void parser_init(Parser *);
+void parser_destroy(Parser *);
 
 #endif
